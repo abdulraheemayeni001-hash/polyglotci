5ef5d12ac7db45471e79eb4a8d8d05a53c895335
abdulraheemayeni001-hash <abdulraheemayeni001@gmail.com>
2026-09-06 00:17:58 +0000

    Deploy hello world Worker to Cloudflare

---
+++ .wrangler/tmp/deploy-XIP8JG/index.js
@@ -0,0 +1,10 @@
+// src/index.ts
+var index_default = {
+  async fetch(request) {
+    return new Response("Hello World!");
+  }
+};
+export {
+  index_default as default
+};
+//# sourceMappingURL=index.js.map
